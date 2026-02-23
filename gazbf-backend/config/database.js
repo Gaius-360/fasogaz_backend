9c1aca8b5830f7b09eee93294b49e609d855598a
Gaïus-360 <gaius2530@gmail.com>
2026-02-23 10:36:12 +0000

    Préparation déploiement Render v2

--- gazbf-backend/config/database.js
+++ gazbf-backend/config/database.js
@@ -17,18 +17,23 @@ module.exports = {
     }
   },
   production: {
-    username: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    dialect: 'mysql',
-    logging: false,
-    pool: {
-      max: 10,
-      min: 0,
-      acquire: 30000,
-      idle: 10000
+  username: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  dialect: 'mysql',
+  logging: false,
+  dialectOptions: {
+    ssl: {
+      rejectUnauthorized: false  // ← nécessaire pour Railway
     }
+  },
+  pool: {
+    max: 10,
+    min: 0,
+    acquire: 30000,
+    idle: 10000
   }
+}
 };
